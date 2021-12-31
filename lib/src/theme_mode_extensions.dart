fbdbb6ad914da2640f8564a39fb53887ee6b3524
Steven Smith <github@icodeforyou.com>
2021-12-30 22:14:12 -0500

    removed thememodetype - didn't belong in 'generic' extensions

--- lib/src/theme_mode_extensions.dart
+++ lib/src/theme_mode_extensions.dart
@@ -1,13 +1,6 @@
 import 'package:extensions_package/extensions_package.dart';
 import 'package:flutter/material.dart';
 
-enum BrightnessType {
-  appDark,
-  appLight,
-  systemDark,
-  systemLight,
-}
-
 extension ThemeModeExtensions on ThemeMode {
   Brightness of(BuildContext context) {
     switch (this) {
@@ -17,17 +10,6 @@ extension ThemeModeExtensions on ThemeMode {
         return Brightness.light;
       case ThemeMode.system:
         return context.platformBrightness;
-    }
-  }
-
-  BrightnessType brightnessType(BuildContext context) {
-    switch (this) {
-      case ThemeMode.dark:
-        return BrightnessType.appDark;
-      case ThemeMode.light:
-        return BrightnessType.appLight;
-      case ThemeMode.system:
-        return context.platformBrightness == Brightness.dark ? BrightnessType.systemDark : BrightnessType.systemLight;
     }
   }
 }
