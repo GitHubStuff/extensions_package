29efc88408ee83afc221add59442c8b99db0fdaf
Steven Smith <github@icodeforyou.com>
2022-03-17 17:31:02 -0400

    updated packages

--- example/lib/modular/theme_and_material_widget.dart
+++ example/lib/modular/theme_and_material_widget.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:language_package/language_package.dart';
+import 'package:rae_asset_package/rae_asset_package.dart';
+import 'package:rae_widgets/rae_widgets.dart';
 import 'package:theme_management/theme_management.dart';
-import 'package:widget_tree_package/widet_tree_package.dart';
 
 class ThemeAndMaterialWidget extends StatelessWidget with WidgetTreeMixin {
   @override
